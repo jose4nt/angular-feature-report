091f2670e13b1e1d6d6d019d0c424f548b263f10
jose4nt <maverick7.23@gmail.com>
2020-09-03 00:23:31 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-feature-report
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-feature-report)
---
+++ src/app/feature-list/feature-list.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+import { features } from '../features';
+
+@Component({
+  selector: 'app-feature-list',
+  templateUrl: './feature-list.component.html',
+  styleUrls: ['./feature-list.component.css']
+})
+export class FeatureListComponent {
+  features = features;
+
+  share() {
+    window.alert('The feature has been shared!');
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
